23cf7f64b282565432ecd04539a821351b832a5a
sseans <41116820+sseans@users.noreply.github.com>
2020-12-28 10:00:07 -0700

    basic weather functionality implemented

---
+++ app.js
@@ -0,0 +1,24 @@
+function weatherBalloon( cityID ) {
+    var key = 'a56b35b061c95a78d4ce7e5d294e0094';
+    fetch('https://api.openweathermap.org/data/2.5/weather?id=' + cityID+ '&appid=' + key)  
+    .then(function(resp) { return resp.json() }) // Convert data to json
+    .then(function(data) {
+      drawWeather(data);
+    })
+    .catch(function() {
+      // catch any errors
+    });
+  }
+  
+  window.onload = function() {
+    weatherBalloon( 2147714 );
+  }
+
+  function drawWeather( d ) {
+	var celcius = Math.round(parseFloat(d.main.temp)-273.15);
+	var fahrenheit = Math.round(((parseFloat(d.main.temp)-273.15)*1.8)+32); 
+	
+	document.getElementById('description').innerHTML = d.weather[0].description;
+	document.getElementById('temp').innerHTML = celcius + '&deg;';
+	document.getElementById('location').innerHTML = d.name;
+}
