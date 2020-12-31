e23cd456976d290e800cadda64649a61e8289346
sseans <41116820+sseans@users.noreply.github.com>
2020-12-30 19:00:05 -0700

    added search functionality and enter keypress

--- app.js
+++ app.js
@@ -1,6 +1,30 @@
-function weatherBalloon( cityID ) {
+// Global Variables
+
+// Selectors
+    // Input Selector
+    const inputField = document.querySelector('#inputField')
+    // Button Selector
+    const inputButton = document.querySelector('#inputButton')
+
+// Event Listeners
+inputButton.addEventListener('click', insertCityName)
+
+inputField.addEventListener('keypress', function(keyPress){
+                                            if (keyPress.key === 'Enter') {
+                                                insertCityName()
+                                            }
+                                        }
+)
+// Functions
+function insertCityName(event) {
+    let cityNameSelected = document.getElementById('inputField').value;
+    weatherBalloon( cityNameSelected )
+    document.getElementById('inputfield').value = '';
+}
+
+function weatherBalloon( cityName ) {
     var key = 'a56b35b061c95a78d4ce7e5d294e0094';
-    fetch('https://api.openweathermap.org/data/2.5/weather?id=' + cityID+ '&appid=' + key)  
+    fetch('https://api.openweathermap.org/data/2.5/weather?q=' + cityName + '&appid=' + key)  
     .then(function(resp) { return resp.json() }) // Convert data to json
     .then(function(data) {
       drawWeather(data);
@@ -9,12 +33,8 @@ function weatherBalloon( cityID ) {
       // catch any errors
     });
   }
-  
-  window.onload = function() {
-    weatherBalloon( 2147714 );
-  }
 
-  function drawWeather( d ) {
+function drawWeather( d ) {
 	var celcius = Math.round(parseFloat(d.main.temp)-273.15);
 	var fahrenheit = Math.round(((parseFloat(d.main.temp)-273.15)*1.8)+32); 
 	
